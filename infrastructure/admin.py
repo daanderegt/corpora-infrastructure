4ac6a8561c8b377c779bf0445846e484acb1a3cd
daanderegt <63785135+daanderegt@users.noreply.github.com>
2021-06-07 13:46:45 +0200

    Add files via upload

--- infrastructure/admin.py
+++ infrastructure/admin.py
@@ -3,7 +3,9 @@ from . import models
 from django.contrib import admin
 from django.contrib.admin.models import LogEntry
 
-# Register your models here.
+
+
+# Logentry is registrated and set to readonly
 class LogEntryAdmin(admin.ModelAdmin):
     readonly_fields = ('content_type',
         'user',
@@ -17,6 +19,9 @@ class LogEntryAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         return False
 
+
+
+#logentry and corpora are registered in admin
 admin.site.register(LogEntry,LogEntryAdmin)
 admin.site.register(models.corpora)
 
--- infrastructure/filter.py
+++ infrastructure/filter.py
@@ -6,34 +6,40 @@ from .models import corpora
 from django import template
 from django import forms
 
+#the filters are created here some filters are created automatically others require an extra method
+
 class CorporaFilter(django_filters.FilterSet):
-    #register = template.Library()
     search_filter = django_filters.CharFilter(
+        #this filter requires an extra method wich can be seen below
         method='my_search_filter',
+        #the widget is added so that the bootstrap css style could be added to the filters
         widget=forms.TextInput(attrs={'class':'form-control me-2'}))
     discipline = django_filters.ChoiceFilter(
+        #this filter does not require an extra method and is just the choices that can be made in discipline
         widget=forms.Select(attrs={'class':'form-select'}),
         choices=corpora.discipline_choices)
     category = django_filters.ChoiceFilter(
+        # this filter does not require an extra method and is just the choices that can be made in category
         widget=forms.Select(attrs={'class': 'form-select'}),
         choices=corpora.categories_choices)
     annotated = django_filters.ChoiceFilter(
         method='annotated_isnull_filter',
+        #this filter does require an extra method
         widget=forms.Select(attrs={'class':'form-select'}),
         choices=(('annotated','annotated'),('not annotated','not annotated'))
     )
+
+    # the model and filters are registered here, the fields metioned below can be seen on the site
     class Meta:
         model = corpora
         fields = ['search_filter','discipline','category','annotated']
 
-
-    #@register.filter(name='search')
+    #This filter searches in name annotated anonymization and research
     def my_search_filter(self, queryset, name, value):
         return corpora.objects.filter(
-            Q(name__icontains=value) | Q(annotated__icontains=value) | Q(annotated__icontains=value) | Q(anonymization__icontains=value) | Q(research__icontains=value)
+            Q(name__icontains=value) |  Q(annotated__icontains=value) | Q(anonymization__icontains=value) | Q(research__icontains=value)
         )
-
-    #@register.filter(name='annotated_isnull_filter')
+    #This filter checks if annotated is empty or not
     def annotated_isnull_filter(self, queryset, name, value):
         if(value=='not_annotated'):
             return corpora.objects.filter(Q(annotated__isnull=True))
